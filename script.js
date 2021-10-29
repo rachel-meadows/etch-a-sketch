bd53b8228340196a476a857e7005779ec67b2e34
Rachel Meadows <rachelnewzealand@gmail.com>
2021-10-30 09:23:33 +1300

    Add color to grid squares on hover

--- script.js
+++ script.js
@@ -7,13 +7,34 @@ function makeGrid (width, height) {
         row.classList.add('gridRow');
         for (x = 0; x < width; x++) {
             let cell = document.createElement('div'); // Make new element div
-            console.log(i);
             row.appendChild(cell);
             cell.classList.add('gridSquare');
         }
     }
+    var gridSquares = document.querySelectorAll(".gridSquare");
+    return gridSquares;
+}
 
+function applyHoverPen(gridSquares) {
+    gridSquares.forEach((gridSquare) => {
+        // This handler will be executed every time the cursor is moved over a different list item
+        gridSquare.addEventListener("mouseover", function( e ) {
+            console.log(gridSquare);
+            // Highlight the mouseover grid square
+            e.target.style.background = `rgba(255, 0, 0, ${0.9})`;
+        });
+
+        /* Playing with removing styles:
+        gridSquare.addEventListener("mouseleave", e => {
+            // Reset the colour after a short delay
+            setTimeout(function() {
+                e.target.style.background = "";
+            }, 500);
+        });
+        */
+    });
 }
 
 
-makeGrid(16, 16); // Width, height. Default should be 16.
+gridSquares = makeGrid(16, 16); // Width, height. Default should be 16.
+applyHoverPen(gridSquares);
