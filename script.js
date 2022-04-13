c25df5c9fe79f54d6732a54637433af20faa1686
Rachel Meadows <rachelnewzealand@gmail.com>
2022-04-14 08:09:24 +1200

    Make the pen work on mobile

--- script.js
+++ script.js
@@ -1,134 +1,135 @@
 // Constants
-const container = document.querySelector('#container');
-const colorChoiceOption = document.getElementsByName('color');
-const opacityChoiceOption = document.getElementsByName('opacity');
-let gridSize = 16;
-let opacityChoice = 1;
-let colorChoice = "1, 1, 1, ";
-let colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`;
-let currentlyDrawing = true;
+const container = document.querySelector('#container')
+const colorChoiceOption = document.getElementsByName('color')
+const opacityChoiceOption = document.getElementsByName('opacity')
+let gridSize = 16
+let opacityChoice = 1
+let colorChoice = '1, 1, 1, '
+let colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
+let currentlyDrawing = true
 
-function makeGrid (width, height) {
-    for (i = 0; i < height; i++){
-        let row = document.createElement('div'); // Make new element div
-        container.appendChild(row);
-        row.classList.add('gridRow');
-        for (x = 0; x < width; x++) {
-            let cell = document.createElement('div'); // Make new element div
-            row.appendChild(cell);
-            cell.classList.add('gridSquare');
-        }
+function makeGrid(width, height) {
+  for (i = 0; i < height; i++) {
+    let row = document.createElement('div') // Make new element div
+    container.appendChild(row)
+    row.classList.add('gridRow')
+    for (x = 0; x < width; x++) {
+      let cell = document.createElement('div') // Make new element div
+      row.appendChild(cell)
+      cell.classList.add('gridSquare')
     }
-    var gridRows = document.querySelectorAll(".gridSquare");
-    var gridSquares = document.querySelectorAll(".gridSquare");
-    return [gridRows, gridSquares];
+  }
+  var gridRows = document.querySelectorAll('.gridSquare')
+  var gridSquares = document.querySelectorAll('.gridSquare')
+  return [gridRows, gridSquares]
 }
 
 function applyHoverPen(gridSquares) {
-    console.log("CurrentlyDrawing state is: " + currentlyDrawing);
-    gridSquares.forEach((gridSquare) => {
-        let cellCount = 0;
-        // This handler will be executed every time the cursor is moved over a different list item
-        gridSquare.addEventListener("mouseover", function( e ) {
-            if (currentlyDrawing == true) {
-                // Highlight the mouseover grid square
-                cellCount += 0.2;
-                colorOpacityChoice = `rgba(${colorChoice + (opacityChoice + cellCount)})`;
-                e.target.style.backgroundColor = colorOpacityChoice;
-            }
-        });
-
-    });
-
-    document.getElementById("container").addEventListener("click", function() {
+  console.log('CurrentlyDrawing state is: ' + currentlyDrawing)
+  gridSquares.forEach((gridSquare) => {
+    let cellCount = 0
+    // These handlers will be executed every time the cursor is moved over a different list item
+    ;['mouseover', 'touchstart'].forEach((evt) =>
+      gridSquare.addEventListener(evt, function (e) {
         if (currentlyDrawing == true) {
-            currentlyDrawing = false;
-        } else {
-            currentlyDrawing = true;
+          // Highlight the mouseover grid square
+          cellCount += 0.2
+          colorOpacityChoice = `rgba(${
+            colorChoice + (opacityChoice + cellCount)
+          })`
+          e.target.style.backgroundColor = colorOpacityChoice
         }
-    });
+      })
+    )
+  })
+
+  document.getElementById('container').addEventListener('click', function () {
+    if (currentlyDrawing == true) {
+      currentlyDrawing = false
+    } else {
+      currentlyDrawing = true
+    }
+  })
 }
 
 function changeColor() {
+  // User inputs custom colour
+  let customColor = document.getElementById('colorPicker')
+  customColor.addEventListener('input', (event) => {
+    // Credit for the below function to https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+    function hexToRGB(hex) {
+      var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16)
+      return `${r}, ${g}, ${b}, `
+    }
 
-    // User inputs custom colour
-    let customColor = document.getElementById("colorPicker");
-    customColor.addEventListener("input",(event)=>{
-            // Credit for the below function to https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
-            function hexToRGB(hex) {
-                var r = parseInt(hex.slice(1, 3), 16),
-                    g = parseInt(hex.slice(3, 5), 16),
-                    b = parseInt(hex.slice(5, 7), 16);
-                return `${r}, ${g}, ${b}, `;
-            }
-            
-            // Hex to RGB
-            colorChoice = hexToRGB(customColor.value);
-            colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`;
-            document.getElementById("custom").value = colorChoice;
-     });
+    // Hex to RGB
+    colorChoice = hexToRGB(customColor.value)
+    colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
+    document.getElementById('custom').value = colorChoice
+  })
 
-    colorChoiceOption.forEach((option) => {
-        // This handler will be executed every time the user clicks a different radio button (other than a custom color)
-        option.addEventListener("click", function( e ) {
-            colorChoice = option.value;
-            colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`;
-        });        
-    });
+  colorChoiceOption.forEach((option) => {
+    // This handler will be executed every time the user clicks a different radio button (other than a custom color)
+    option.addEventListener('click', function (e) {
+      colorChoice = option.value
+      colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
+    })
+  })
 
-    opacityChoiceOption.forEach((option) => {
-        // This handler will be executed every time the user clicks a different radio button
-        option.addEventListener("click", function( e ) {
-            if (option.id == "fade") {
-                opacityChoice = 0.1;
-            } else {
-                opacityChoice = 1;
-            }
-            colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`;
-        });        
-    });
+  opacityChoiceOption.forEach((option) => {
+    // This handler will be executed every time the user clicks a different radio button
+    option.addEventListener('click', function (e) {
+      if (option.id == 'fade') {
+        opacityChoice = 0.1
+      } else {
+        opacityChoice = 1
+      }
+      colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
+    })
+  })
 }
 
 function clearGrid(gridSquares) {
-    const clearGrid = document.querySelector('#clearGrid');
-    clearGrid.addEventListener("click", function( e ) {
-        backgroundColor = `rgba(242, 252, 253, 1)`;
-        gridSquares.forEach((gridSquare) => {
-            gridSquare.style.backgroundColor = backgroundColor;
-        });
+  const clearGrid = document.querySelector('#clearGrid')
+  clearGrid.addEventListener('click', function (e) {
+    backgroundColor = `rgba(242, 252, 253, 1)`
+    gridSquares.forEach((gridSquare) => {
+      gridSquare.style.backgroundColor = backgroundColor
+    })
 
-        document.getElementById("black").checked = true;
-        document.getElementById("solid").checked = true;
-        opacityChoice = 1;
-        colorChoice = "1, 1, 1, ";
-        colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`;
-
-    });
+    document.getElementById('black').checked = true
+    document.getElementById('solid').checked = true
+    opacityChoice = 1
+    colorChoice = '1, 1, 1, '
+    colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
+  })
 }
 
 function resizeGrid() {
-    let slider = document.getElementById("myRange");
-    let output = document.getElementById("gridSize");
-    output.innerHTML = slider.value; // Display default slider value
-    
-    slider.oninput = function() {
-        gridSize = this.value;
-        output.innerHTML = this.value; // Display new slider value
-        // Get rid of the old grid
-        container.querySelectorAll('*').forEach(n => n.remove());
-        // Make the new grid
-        enterActiveState(gridSize);
-    }
+  let slider = document.getElementById('myRange')
+  let output = document.getElementById('gridSize')
+  output.innerHTML = slider.value // Display default slider value
+
+  slider.oninput = function () {
+    gridSize = this.value
+    output.innerHTML = this.value // Display new slider value
+    // Get rid of the old grid
+    container.querySelectorAll('*').forEach((n) => n.remove())
+    // Make the new grid
+    enterActiveState(gridSize)
+  }
 }
 
-function enterActiveState (gridSize=16) {
-    gridArray = makeGrid(gridSize, gridSize); // Width, height - so needn't be square if a rectangle is wanted
-    let gridSquares = gridArray[1];
-    applyHoverPen(gridSquares);
-    clearGrid(gridSquares);
-    resizeGrid();
-    changeColor();
+function enterActiveState(gridSize = 16) {
+  gridArray = makeGrid(gridSize, gridSize) // Width, height - so needn't be square if a rectangle is wanted
+  let gridSquares = gridArray[1]
+  applyHoverPen(gridSquares)
+  clearGrid(gridSquares)
+  resizeGrid()
+  changeColor()
 }
 
 // Calling the functions!
-enterActiveState();
+enterActiveState()
