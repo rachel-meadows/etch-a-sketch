e1f12b03a62ec0b5f37133a9ddec5ea2213a1eb6
Rachel Meadows <rachelnewzealand@gmail.com>
2022-04-14 12:33:34 +1200

    Change background styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Constants
+// Initialisations and constants
 const container = document.querySelector('#container')
 const colorChoiceOption = document.getElementsByName('color')
 const opacityChoiceOption = document.getElementsByName('opacity')
@@ -8,30 +8,31 @@ let colorChoice = '1, 1, 1, '
 let colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
 let currentlyDrawing = true
 
+// Dynamically create a matrix
 function makeGrid(width, height) {
   for (i = 0; i < height; i++) {
-    let row = document.createElement('div') // Make new element div
+    let row = document.createElement('div')
     container.appendChild(row)
     row.classList.add('gridRow')
     for (x = 0; x < width; x++) {
-      let cell = document.createElement('div') // Make new element div
+      let cell = document.createElement('div')
       row.appendChild(cell)
       cell.classList.add('gridSquare')
     }
   }
-  var gridRows = document.querySelectorAll('.gridSquare')
-  var gridSquares = document.querySelectorAll('.gridSquare')
+  let gridRows = document.querySelectorAll('.gridSquare')
+  let gridSquares = document.querySelectorAll('.gridSquare')
   return [gridRows, gridSquares]
 }
 
+// Draw on the grid
 function applyHoverPen(gridSquares) {
-  console.log('CurrentlyDrawing state is: ' + currentlyDrawing)
   gridSquares.forEach((gridSquare) => {
     let cellCount = 0
-    // These handlers will be executed every time the cursor is moved over a different list item
-    ;['mouseover', 'touchstart'].forEach((evt) =>
-      gridSquare.addEventListener(evt, function (e) {
-        if (currentlyDrawing == true) {
+    // Handlers for hover and touchscreen (mobile)
+    ;['mouseover', 'touchstart'].forEach((action) =>
+      gridSquare.addEventListener(action, (e) => {
+        if (currentlyDrawing === true) {
           // Highlight the mouseover grid square
           cellCount += 0.2
           colorOpacityChoice = `rgba(${
@@ -43,8 +44,9 @@ function applyHoverPen(gridSquares) {
     )
   })
 
+  // Toggle pen on and off
   document.getElementById('container').addEventListener('click', function () {
-    if (currentlyDrawing == true) {
+    if (currentlyDrawing === true) {
       currentlyDrawing = false
     } else {
       currentlyDrawing = true
@@ -53,10 +55,10 @@ function applyHoverPen(gridSquares) {
 }
 
 function changeColor() {
-  // User inputs custom colour
+  // Handle user inputting a custom colour
   let customColor = document.getElementById('colorPicker')
-  customColor.addEventListener('input', (event) => {
-    // Credit for the below function to https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+  customColor.addEventListener('input', () => {
+    // Credit for hexToRGB function: https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
     function hexToRGB(hex) {
       var r = parseInt(hex.slice(1, 3), 16),
         g = parseInt(hex.slice(3, 5), 16),
@@ -64,23 +66,22 @@ function changeColor() {
       return `${r}, ${g}, ${b}, `
     }
 
-    // Hex to RGB
     colorChoice = hexToRGB(customColor.value)
     colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
     document.getElementById('custom').value = colorChoice
   })
 
+  // Change pen to a default colour
   colorChoiceOption.forEach((option) => {
-    // This handler will be executed every time the user clicks a different radio button (other than a custom color)
-    option.addEventListener('click', function (e) {
+    option.addEventListener('click', () => {
       colorChoice = option.value
       colorOpacityChoice = `rgba(${colorChoice + opacityChoice})`
     })
   })
 
+  // Change pen to a new opacity level
   opacityChoiceOption.forEach((option) => {
-    // This handler will be executed every time the user clicks a different radio button
-    option.addEventListener('click', function (e) {
+    option.addEventListener('click', () => {
       if (option.id == 'fade') {
         opacityChoice = 0.1
       } else {
@@ -91,9 +92,10 @@ function changeColor() {
   })
 }
 
+// Reset the grid
 function clearGrid(gridSquares) {
   const clearGrid = document.querySelector('#clearGrid')
-  clearGrid.addEventListener('click', function (e) {
+  clearGrid.addEventListener('click', () => {
     backgroundColor = `rgba(242, 252, 253, 1)`
     gridSquares.forEach((gridSquare) => {
       gridSquare.style.backgroundColor = backgroundColor
@@ -123,7 +125,8 @@ function resizeGrid() {
 }
 
 function enterActiveState(gridSize = 16) {
-  gridArray = makeGrid(gridSize, gridSize) // Width, height - so needn't be square if a rectangle is wanted
+  // makeGrid args are width, height - so needn't be square if a rectangle is wanted
+  gridArray = makeGrid(gridSize, gridSize)
   let gridSquares = gridArray[1]
   applyHoverPen(gridSquares)
   clearGrid(gridSquares)
@@ -131,5 +134,4 @@ function enterActiveState(gridSize = 16) {
   changeColor()
 }
 
-// Calling the functions!
 enterActiveState()
